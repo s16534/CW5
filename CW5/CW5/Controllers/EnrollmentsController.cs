e6ba7600ff6bf1237dcb0eeece0daf054fc8b2bf
s16534 <s16534@pjwstk.edu.pl>
2020-05-06 18:12:08 +0200

    Dodanie projektu

---
+++ CW5/CW5/Controllers/EnrollmentsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using CW5.DTOs.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CW5.Controllers
+{
+    [Route("api/enrollments")]
+    [ApiController]
+    public class EnrollmentsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult EnrollStudent(EnrollStudentRequest request)
+        {
+            /*
+            using(var con = new SqlConnection(""))
+            using(var com = new SqlCommand())
+            {
+                com.Connection = con;
+                con.Open();
+                var tran = con.BeginTransaction();
+                com.CommandText = "SELECT IdStudies FROM studies WHERE name = @name";
+                com.Parameters.AddWithValue("name", request.Studies);
+
+                var er = com.ExecuteReader();
+
+            }*/
+            return Ok();
+        }
+    }
+}
---
+++ CW5/CW5/DTOs/Requests/EnrollStudentRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CW5.DTOs.Requests
+{
+    public class EnrollStudentRequest
+    {
+        public string IndexNumber { get; set; }
+
+        [Required] 
+        [MaxLength(15)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string LastName{ get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        [Required]
+        public string Studies{ get; set; }
+
+    }
+}
